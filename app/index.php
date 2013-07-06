6f55cafbd702f450a8f5422cd13b12ab11eab1ae
Nicola Peduzzi <thenikso@gmail.com>
2013-07-06 17:11:30 +0200

    Using jQuery 1.10.1 to support IE8

--- app/index.php
+++ app/index.php
@@ -32,7 +32,7 @@
       data: <?php echo ng_current_page_data(); ?>
     };
     </script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
