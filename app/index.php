da76912c1799c1e183bc35fc698b80a14b554cac
Nicola Peduzzi <thenikso@gmail.com>
2013-10-02 14:17:19 -0700

    Removed unused menu link

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
 								</a>
 							</h1>
 						</li>
-						<li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+						<li class="toggle-topbar menu-icon"><a href=""><span></span></a></li>
 					</ul>
 
 					<section class="top-bar-section">
